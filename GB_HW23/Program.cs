c63e7c31cc18624378159c73e2435c4a65bd2a10
hannmx <mxkud@mail.ru>
2022-11-20 18:35:23 +0300

    Создал программы к ДЗ 19, 21, 23

---
+++ GB_HW23/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите целое натуральное число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Таблица кубов от 1 до " + num + " равна:");
+double[] arr = new double[num];
+int i = 0;
+while(i < num)
+{
+    arr[i] = i + 1;
+    arr[i] = Math.Pow(arr[i], 3); 
+    Console.Write(arr[i] + " ");
+    i++;
+}
